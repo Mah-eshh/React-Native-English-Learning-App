bbcd6c9b7f70dbbfe1a2f7322dca97e12337a357
Mah-eshh <92861@fhss.sjp.ac.lk>
2021-02-14 17:40:10 +0530

    initial

--- screens/MainTabScreen.js
+++ screens/MainTabScreen.js
@@ -1,5 +1,4 @@
 import React from 'react';
-
 import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
 import { createStackNavigator } from '@react-navigation/stack';
 
