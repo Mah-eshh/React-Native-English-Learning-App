3c23cc05763f90f71f06f5e5e27c1327d66623f7
Mah-eshh <92861@fhss.sjp.ac.lk>
2021-02-16 19:02:56 +0530

    color

--- drawer/web/WebBrowserScreen.js
+++ drawer/web/WebBrowserScreen.js
@@ -9,36 +9,36 @@ export default class WebBrowserScreen extends Component {
     return (
       <View style={styles.container}>
        <FontAwesome.Button name="youtube" 
-          onPress={this.YouTube} style={{ width: 150, backgroundColor: '#FD5580', alignItems: 'center', justifyContent: 'center' }}>
-         Youtube</FontAwesome.Button> 
+          onPress={this.YouTube} style={{ width: 120, backgroundColor: '#ff0000', alignItems: 'center', justifyContent: 'flex-start' }}>
+            Youtube</FontAwesome.Button> 
          
          <FontAwesome.Button name="google" backgroundColor="#6495ed" 
-        onPress={this.Google}>
+        onPress={this.Google} style={{ width: 220, backgroundColor: '#48d1cc0', alignItems: 'center', justifyContent: 'center' }}>
          Google Translate </FontAwesome.Button>
 
-         <FontAwesome.Button name="google" backgroundColor="#6495ed" 
-        onPress={this.Google}>
+         <FontAwesome.Button name="search" 
+        onPress={this.Google} style={{ width: 220, backgroundColor: '#f08080', alignItems: 'center', justifyContent: 'center' }}>
          Oxford Dictionary </FontAwesome.Button>
          
        <FontAwesome.Button name="youtube" backgroundColor="red" 
-          onPress={this.YouTubeDrama}>
+          onPress={this.YouTubeDrama} style={{ width: 220, backgroundColor: '#ff0000', alignItems: 'center', justifyContent: 'center' }}>
          English Drama series</FontAwesome.Button> 
 
           <FontAwesome.Button name="youtube" backgroundColor="red" 
-          onPress={this.YouTubeSongs}>
+          onPress={this.YouTubeSongs} style={{ width: 220, backgroundColor: '#ff0000', alignItems: 'center', justifyContent: 'center' }}>
          English Songs        </FontAwesome.Button> 
 
 
         
   
-       <FontAwesome.Button name="wordpress" backgroundColor="#6495ed" 
-          onPress={this.englishGrammar}>
+       <FontAwesome.Button name="wordpress" 
+          onPress={this.englishGrammar} style={{ width: 220, backgroundColor: '#4b0082', alignItems: 'center', justifyContent: 'center' }}>
           English Grammar lessons
           </FontAwesome.Button>
         
        
-       <FontAwesome.Button name="wordpress" backgroundColor="#6495ed" 
-         onPress={this.englishVocabulary} style={{ width: 250, backgroundColor: '#F26', alignItems: 'center', justifyContent: 'center' }}>
+       <FontAwesome.Button name="wordpress" 
+         onPress={this.englishVocabulary} style={{ width: 220, backgroundColor: '#4b0082', alignItems: 'center', justifyContent: 'center' }}>
           English Vocabulary            </FontAwesome.Button>
         
       </View>
@@ -76,7 +76,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'space-evenly',
     paddingTop: Constants.statusBarHeight,
-    backgroundColor: '#ae78',
+    backgroundColor: '#87cefa',
   },
   button: {
     marginVertical: 10,
