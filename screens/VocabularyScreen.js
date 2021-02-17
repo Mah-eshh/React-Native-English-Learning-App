324b95d5ae960dbb228a3cdde2477fe707cfbdae
Mah-eshh <92861@fhss.sjp.ac.lk>
2021-02-17 15:57:30 +0530

    added

--- screens/VocabularyScreen.js
+++ screens/VocabularyScreen.js
@@ -4,14 +4,19 @@ const { width, height } = Dimensions.get('screen');
 
 const data = [
     'https://lessonsforenglish.com/wp-content/uploads/2019/12/100-Most-Common-Phrasal-Verbs-List.png',
-    'https://lessonsforenglish.com/wp-content/uploads/2020/05/Most-Important-Opposite-Words-List.png', 
-    'https://lessonsforenglish.com/wp-content/uploads/2020/05/900-Opposite-Words-in-English-1.png',
-    'https://lessonsforenglish.com/wp-content/uploads/2020/05/Basic-and-Detailed-Opposite-Words-List.png',
-    'https://lessonsforenglish.com/wp-content/uploads/2020/06/360-Synonyms-Words-List-in-English.png',
-    'https://lessonsforenglish.com/wp-content/uploads/2020/05/Most-Important-Opposite-Words-List.png',
-    'https://lessonsforenglish.com/wp-content/uploads/2020/05/160-Synonym-Words-List-in-English.png',
     'https://lessonsforenglish.com/wp-content/uploads/2019/12/Most-Common-Phrasal-Verbs-List.png', 
+    'https://lessonsforenglish.com/wp-content/uploads/2019/12/200-IELTS-Words-You-Need-to-Know.png',
+    'https://lessonsforenglish.com/wp-content/uploads/2019/12/Adjectives-List-Adjectives-Vocabulary-Word-List-.png',
+    'https://lessonsforenglish.com/wp-content/uploads/2019/12/Verbs-Followed-By-GERUNDS.png',
+    'https://lessonsforenglish.com/wp-content/uploads/2021/01/Adverbs-of-Place-Definition-and-36-Example-Words.png',
+    'https://lessonsforenglish.com/wp-content/uploads/2019/12/Synonym-Words-Pretty-English-Vocabulary.png',
+    'https://lessonsforenglish.com/wp-content/uploads/2020/02/Synonym-Words-with-STRONG.jpg',
+    'https://lessonsforenglish.com/wp-content/uploads/2019/12/Silent-Letters-List.png',
+    'https://lessonsforenglish.com/wp-content/uploads/2019/12/Homonym-Words.png',
+    'https://lessonsforenglish.com/wp-content/uploads/2020/06/360-Synonyms-Words-List-in-English.png',
     
+    'https://lessonsforenglish.com/wp-content/uploads/2020/04/Irregular-Plurals-Singular-and-Plural.png',
+    'https://lessonsforenglish.com/wp-content/uploads/2020/05/160-Synonym-Words-List-in-English.png',
 ];
  
 
